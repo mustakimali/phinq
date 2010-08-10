2e4ea8006915353542e8efe270e05f2e3b3c5364
t.montg <t.montg@03b1a93e-dc9c-f995-7c39-be2af083c618>
2010-08-10 13:26:45 +0000

    made Util::compare() less branchy

--- src/Phinq/Util.php
+++ src/Phinq/Util.php
@@ -25,23 +25,11 @@
 		public static function compare($a, $b) {
 			if (is_int($a) || is_float($a)) {
 				return (is_int($b) || is_float($b)) && $a == $b ? 0 : ($a < $b ? -1 : 1);
-			} else if ($a === null) {
-				return $b === null ? 0 : ($a < $b ? -1 : 1);
-			} else if (is_string($a)) {
-				return is_string($b) && $a === $b ? 0 : ($a < $b ? -1 : 1);
 			} else if (is_object($a)) {
-				return is_object($b) && $a === $b ? 0 : 1; //reference equals
-			} else if (is_array($a)) {
-				return is_array($b) && $a === $b ? 0 : ($a < $b ? -1 : 1); //keys must match
-			} else if (is_resource($a)) {
-				return is_resource($b) && $a === $b ? 0 : ($a < $b ? -1 : 1);
-			
-			//@codeCoverageIgnoreStart
+				return is_object($b) && $a === $b ? 0 : 1; //can't cast object to int
 			} else {
-				//this is just a safeguard in case PHP adds more types or something
-				return $a === $b ? 0 : ($a < $b ? -1 : 1);
+				return gettype($a) === gettype($b) && $a === $b ? 0 : ($a < $b ? -1 : 1);
 			}
-			//@codeCoverageIgnoreEnd
 		}
 
 		public static function convertToNumericallyIndexedArray($collection) {
@@ -76,5 +64,5 @@
 		}
 
 	}
-	
+
 ?>
