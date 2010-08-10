3a20e42d1e7d3c4eb7b41a1e9ebbe1ea406fd6ac
t.montg <t.montg@03b1a93e-dc9c-f995-7c39-be2af083c618>
2010-08-10 13:39:46 +0000

    made all the non-lambda related queries use the query factory

--- src/Phinq/ReverseQuery.php
+++ src/Phinq/ReverseQuery.php
@@ -4,6 +4,14 @@
 
 	class ReverseQuery implements Query {
 
+		/**
+		 * Just a dummy constructor so that we can instantiate it via
+		 * reflection without doing something annoying
+		 *
+		 * @ignore
+		 */
+		public function __construct() {}
+
 		public function execute(array $collection) {
 			return array_reverse($collection);
 		}
