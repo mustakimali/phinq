55b53d81fd95d98d5250cdf404a3d0ed76c973a2
t.montg <t.montg@03b1a93e-dc9c-f995-7c39-be2af083c618>
2010-08-13 11:46:06 +0000

    undid everything that had been done in this branch because it sucked

--- branches/2.0/src/Phinq/OrderedPhinq.php
+++ branches/2.0/src/Phinq/OrderedPhinq.php
@@ -6,22 +6,18 @@
 
 	class OrderedPhinq extends Phinq {
 
-		public function __construct($collection, QueryFactory $queryFactory, array $queries) {
-			parent::__construct($collection, $queryFactory, $queries);
-		}
-
 		/**
 		 * Performs a subsequent sort
 		 *
-		 * @param mixed $expression
+		 * @param Closure $lambda
 		 * @param bool $descending Whether to sort in descending order
 		 * @return OrderedPhinq
 		 */
-		public function thenBy($expression, $descending = false) {
-			$this->addToQueue($this->getQueryFactory()->getQuery(QueryType::ThenBy, array($this->getLastQuery(), $expression, $descending)));
+		public function thenBy(Closure $lambda, $descending = false) {
+			$this->addToQueue(new ThenByQuery($this->getLastQuery(), $lambda, $descending));
 			return $this;
 		}
 
 	}
-	
+
 ?>
